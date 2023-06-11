a5058e0451d7a9f79bd20612365e821e624c3cd9
comrade-tea_github <vasil@dotorg.ru>
2023-06-11 16:53:39 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
-const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === "  production"
 const basePath = isProduction ? "/react-videoplayer/" : "./";
 export default defineConfig({
-  plugins: [react()],
   base: basePath,
-  build: {env: {basePath}},
+  plugins: [react()],
 })
