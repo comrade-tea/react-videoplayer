ecc2a75838a0d37a9bbe14cc06ce107fe8498cb4
comrade-tea_github <vasil@dotorg.ru>
2023-06-11 12:35:25 +0300

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+<h3>Features:</h3> 
+<li>Selecting a video by changing the src of the video</li>
+<li>Configuring video options</li>
+<li>Custom video controls that behave like native ones (click/drag)</li>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+const isProduction = process.env.NODE_ENV === "production"
 export default defineConfig({
   plugins: [react()],
+  base: isProduction ? "/react-videoplayer/" : "./"
 })
