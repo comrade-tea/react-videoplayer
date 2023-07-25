0c4004511be5c66943b72ae79aad44e9b9438d83
comrade-tea_github <vasil@dotorg.ru>
2023-07-25 14:00:03 +0300

    forms for netify

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,56 @@
-import {useCallback} from "react";
+import {useCallback, useState} from "react";
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
 
 const Form = () => {
-    const onFormSubmit = useCallback((event) => {
-        event.preventDefault();
+    const [data, setData] = useState({name: "", email: ""});
 
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-
+    const handleSubmit = (e) => {
         fetch("/", {
             method: "POST",
             headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: new URLSearchParams(formData).toString(),
+            body: encode({"form-name": "contact", ...data})
         })
-            .then(() => console.log("Form successfully submitted"))
-            .catch((error) => alert(error));
-    }, []);
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    const handleChange = e => setData({...data, [e.target.name]: e.target.value});
 
     return (
         <form data-netlify="true"
               name={"contact"}
-              id="form"
               method={"POST"}
-              className={"fixed top-20 right-10 bg-amber-100 p-4 border-2 border-amber-400"} 
-              onSubmit={onFormSubmit}>
+              className={"fixed top-20 right-10 bg-amber-100 p-4 border-2 border-amber-400"}
+              onSubmit={handleSubmit}>
+
+            <input type="hidden" name="form-name" value="contact"/>
 
             <ul className={"flex flex-col gap-4"}>
-                <li><input className="border-2" type="text" name={"name"} placeholder={"name"}/></li>
-                <li><input className="border-2" type="email" name={"email"} placeholder={"email"}/></li>
+                <li>
+                    <input className="border-2"
+                           onChange={handleChange}
+                           type="text"
+                           name={"name"}
+                           value={data.name}
+                           placeholder={"name"}/>
+                </li>
+                <li>
+                    <input className="border-2"
+                           onChange={handleChange}
+                           type="email"
+                           name={"email"}
+                           value={data.email}
+                           placeholder={"email"}
+                    />
+                </li>
             </ul>
 
             <button className={"bg-green-400 py-2 px-6 mt-8"} type={"submit"}>send</button>
